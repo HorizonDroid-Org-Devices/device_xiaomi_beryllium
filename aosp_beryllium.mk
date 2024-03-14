9cfb063804bb3853a98bfea9acb9d44d24b2a0db
ihklknz <haikalluthfianinobalukia@proton.me>
2024-03-14 15:47:53 +0000

    beryllium: HorizonDroid Go Official
    Signed-off-by: ihklknz <haikalluthfianinobalukia@proton.me>

--- aosp_beryllium.mk
+++ aosp_beryllium.mk
@@ -11,12 +11,15 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o_mr1.mk
 
 $(call inherit-product, device/xiaomi/beryllium/device.mk)
 
-# Inherit some common Awaken stuff.
-$(call inherit-product, vendor/awaken/config/common_full_phone.mk)
+# Inherit some common HorizonDroid stuff.
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+
+# Horizon Flags
+HORIZON_MAINTAINER := hklknzツ
 TARGET_BOOT_ANIMATION_RES := 1080
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := awaken_beryllium
+PRODUCT_NAME := aosp_beryllium
 PRODUCT_DEVICE := beryllium
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := POCO F1
